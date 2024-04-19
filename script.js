c42d0e05d372095b64edb4b39991cbaea5236350
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-04-20 00:34:42 +0530

    feat: implement add book, remove book, and change status logic

--- script.js
+++ script.js
@@ -9,35 +9,77 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+Book.prototype.toggleReadStatus = function() {
+  this.read = !this.read;
+}
+
 function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
 
-function displayBook(book) {
+function updateButtonContent(button, read) {
+  isRead = read === true ? "Read" : "Not Read";
+  button.textContent = isRead;
+}
+
+function createStatusButton(book) {
+  const button = document.createElement("button");
+  updateButtonContent(button, book.read);
+  button.classList.add("status");
+
+  button.addEventListener("click", () => {
+    book.toggleReadStatus();
+    updateButtonContent(button, book.read);
+  });
+
+  return button;
+}
+
+function createDeleteButton() {
+  const div = document.createElement("div");
+  div.classList.add("delete-btn");
+
+  div.addEventListener("click", (e) => {
+    const tr = e.target.parentNode.parentNode;
+    myLibrary.splice(tr.getAttribute("data-index"), 1);
+    tr.remove();
+  });
+
+  return div;
+}
+
+function displayBook(book, index) {
   const tr = document.createElement("tr"); 
   const tds = [];
-  for (let i = 0; i < 4; i++) {
+
+  tr.setAttribute("data-index", index);
+  for (let i = 0; i < 5; i++) {
     tds.push(document.createElement("td"));
   }
 
   tds[0].textContent = book.title;
   tds[1].textContent = book.author;
   tds[2].textContent = book.pages;
-  tds[3].textContent = book.read === true ? "Yes" : "No";
 
+  tds[3].appendChild(createStatusButton(book));
+  tds[4].appendChild(createDeleteButton());  
 
   tds.forEach(td => tr.appendChild(td));
   table.appendChild(tr);
 }
 
 function displayAllBooks() {
-  for (const book of myLibrary) {
-    displayBook(book);
-  }
+  myLibrary.forEach((book, index) => {
+    displayBook(book, index);
+  });
 }
 
-addBookButton.addEventListener("click", () => {
+addBookButton.addEventListener("click", (e) => {
   const title = document.querySelector("input[name='title']").value;
+  if (!title) {
+    return;
+  }
+  e.preventDefault();
   const author = document.querySelector("input[name='author']").value;
   const pages = document.querySelector("input[name='pages']").value;
   const read = document.querySelector("select[name='status']").value === "true";
@@ -45,6 +87,8 @@ addBookButton.addEventListener("click", () => {
   addBookToLibrary(title, author, pages, read);
   displayBook(myLibrary.at(-1));
 });
+
+
 
 // sample filler data
 addBookToLibrary("Lord of the Rings", "J.R.R. Tolkein", 1147, true);
